b547d6d520aeef06962ae2f39ef718cfb6f6549f
ad-tra <you@example.com>
2022-04-11 00:46:09 -0400

    added partial multimedia

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,12 @@
 export default function About(){
     return (
         <div className="about-container">
-            <p> Hello there, I'm <a href= "https://adtra.me"className="link">Adam</a>I made TOTH in my senior year of high school because I was bored and because I wanted to apply what I learned in my CS classes.</p>
+            <p> Hello there, I'm <a href= "https://adtra.me"className="link"> Adam </a>I made TOTH in my senior year of high school because I was bored and because I wanted to apply what I learned in my CS classes.</p>
             <p>Since It was my first programming side project, it was crappy. I didn't know any better. TOTH didn't look bad in terms of UI and UX, but a lot of the logic for fetching content was inconsistent and fragile, which caused random bugs and frequent irritations. Hopefully, now I know a thing or two on how to make a stable site. I'll rewrite TOTH to make it better and smoother. It will help students bypass SAT's agony</p>
+            <h2>Live Demo</h2>
+            <p>here is a run down of how the TOTH works and how you can use it to prepare for the reading section of the SAT</p>
+            <div style={{position: 'relative', marginTop: '1em', paddingBottom: '56.25%', height: 0}}><iframe src="https://www.loom.com/embed/6df7eb7b9aa6433c924d2b8f24019bcb?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true" frameBorder={0} webkitallowfullscreen mozallowfullscreen allowFullScreen style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} /></div>
+
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -9,57 +9,73 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-      <main id="index" data-barba="container" data-barba-namespace="index">
-        <div className="left-panel">
-          <h1> Prepare for <br />the SAT Reading<br /></h1>
-          <noscript>&lt;h1 style= "visibility: visible;"&gt; Prepare for &lt;br&gt;the SAT Reading&lt;/br&gt;&lt;/h1&gt;</noscript>
-          <div className="dash" />
-        </div>
-        <div className="right-panel">
-          <h2 className="medium-text">Become a stronger reader <span> by exposing yourself to challenging SAT passages. </span></h2>
-          <noscript>&lt;h2 class = "medium-text" style = "visibility: visible;"&gt;Become a stronger reader &lt;span&gt; by exposing yourself to challenging SAT passages. &lt;/span&gt;&lt;/h2&gt;</noscript>
-          <Link href="./literature"><a className="button" >Start Reading</a></Link>
-          <div className="button-pattern" />	
-        </div>	
-        <div className="pros-container">
-          <div className="pro pro1">
-            <h3>Practice your reading skills with a rich collection of SAT passages</h3>
-            <p>Discover the easy English behind Standardized Tests' verbose sentences and unfamiliar vocabulary. Here at TOTH, you can practice and improve your SAT reading score by reading challenging articles in History, Science, Social Science, and Literature.</p>
-          </div>
-          <div className="pro-1-thick-dash" />
-          <div className="pro-2-thick-dash" />
-          <div className="pro pro2">
-            <h3>Master the passage before answering the questions</h3>
-            <p>TOTH is a reading platform that focuses on understanding the text before diving into the questions. If you have a hard time understanding what the author is talking about, TOTH is perfect for you. You'll get to practice through articles that follow SAT structure and feel.</p>	
-          </div>
-        </div>
-        <div className="testo-container">
-          <h2>Students love it!</h2>
-          <div className="testo">
+        <main id="index" data-barba="container" data-barba-namespace="index">
+          <div className="left-panel">
+            <h1> Prepare for <br />the SAT Reading<br /></h1>
+            <noscript>&lt;h1 style= "visibility: visible;"&gt; Prepare for &lt;br&gt;the SAT Reading&lt;/br&gt;&lt;/h1&gt;</noscript>
             <div className="dash" />
-            <p className="medium-text">wow this is so helpful!<span>I was looking for something 
-                like this since, I'm trying to passively study reading for 
+          </div>
+          <div className="right-panel">
+            <h2 className="medium-text">Become a stronger reader <span> by exposing yourself to challenging SAT passages. </span></h2>
+            <noscript>&lt;h2 class = "medium-text" style = "visibility: visible;"&gt;Become a stronger reader &lt;span&gt; by exposing yourself to challenging SAT passages. &lt;/span&gt;&lt;/h2&gt;</noscript>
+            <Link href="./literature"><a className="button" >Start Reading</a></Link>
+            <div className="button-pattern" />
+          </div>
+          <div className="pros-container">
+            <div className="pro pro1">
+              <h3>Practice your reading skills with a rich collection of SAT passages</h3>
+              <p>Discover the easy English behind Standardized Tests' verbose sentences and unfamiliar vocabulary. Here at TOTH, you can practice and improve your SAT reading score by reading challenging articles in History, Science, Social Science, and Literature.</p>
+            </div>
+            <div className="pro-1-thick-dash" />
+            <div className="pro-2-thick-dash" />
+            <div className="pro pro2">
+              <h3>Master the passage before answering the questions</h3>
+              <p>TOTH is a reading platform that focuses on understanding the text before diving into the questions. If you have a hard time understanding what the author is talking about, TOTH is perfect for you. You'll get to practice through articles that follow SAT structure and feel.</p>
+            </div>
+          </div>
+          <div className="testo-container">
+            <h2>Students love it!</h2>
+            <div className="testo">
+              <div className="dash" />
+              <p className="medium-text">wow this is so helpful!<span>I was looking for something
+                like this since, I'm trying to passively study reading for
                 2 months before I go in depth, and </span>this is perfect!</p>
+            </div>
+            <p className="testo-credit  medium-text"><span>u/milly_7</span></p>
+            <div className="testo">
+              <div className="dash" />
+              <p className="medium-text"><span>This is literally </span>a lifesaver.<span> I've struggled with reading for so long now.</span></p>
+            </div>
+            <p className=" testo-credit medium-text"><span>u/Visual_Plate</span></p>
+            <div className="testo">
+              <div className="dash" />
+              <p className="medium-text"><span>I could never understand what the hell history and science authors are trying to say. But, </span>Toth improved my reading sense.<span> Thank you.</span></p>
+            </div>
+            <p className=" testo-credit medium-text"><span>Eden Collins</span></p>
+            <div className="testo">
+              <div className="dash" />
+              <p className="medium-text">
+                <figure>
+                  <audio
+                    controls
+                    style={{width: '100%'}}
+                    src="/great_success.mp3">
+                    Your browser does not support the
+                    <code>audio</code> element.
+                  </audio>
+                </figure>
+
+              </p>
+            </div>
+            <p className=" testo-credit medium-text"><span>Borat</span></p>
           </div>
-          <p className="testo-credit  medium-text"><span>u/milly_7</span></p>
-          <div className="testo">
-            <div className="dash" />
-            <p className="medium-text"><span>This is literally </span>a lifesaver.<span> I've struggled with reading for so long now.</span></p>
-          </div>
-          <p className=" testo-credit medium-text"><span>u/Visual_Plate</span></p>
-          <div className="testo">
-            <div className="dash" />
-            <p className="medium-text"><span>I could never understand what the hell history and science authors are trying to say. But, </span>Toth improved my reading sense.<span> Thank you.</span></p>
-          </div>
-          <p className=" testo-credit medium-text"><span>Eden Collins</span></p>
-        </div>
-        <Link href="./literature"><a className="button recall-button" >Start Reading</a></Link> 
-        <p className="button-recall-disc">Get going, It's free</p>
-      </main>	
-      <footer></footer>
-    </div>
+          <Link href="./literature"><a className="button recall-button" >Start Reading</a></Link>
+          <p className="button-recall-disc">Get going, It's free</p>
+        </main>
+        <footer></footer>
+      </div>
 
 
-      </>
+    </>
   )
 }
