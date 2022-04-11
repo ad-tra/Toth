9d7768870957a463c25b1f00a5d66fe48d7526a6
ad-tra <you@example.com>
2022-04-11 01:06:06 -0400

    added dumb slideshow

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,9 @@ export default function About(){
             <h2>Live Demo</h2>
             <p>here is a run down of how the TOTH works and how you can use it to prepare for the reading section of the SAT</p>
             <div style={{position: 'relative', marginTop: '1em', paddingBottom: '56.25%', height: 0}}><iframe src="https://www.loom.com/embed/6df7eb7b9aa6433c924d2b8f24019bcb?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true" frameBorder={0} webkitallowfullscreen mozallowfullscreen allowFullScreen style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} /></div>
+            <h2>How I built TOTH</h2>
+            <p>I first designed the website on Adobe XD, and then when i was happy with the look and feel of the site, I started development. I used vscode to develop the site.  It currently runs on node.js, Next/React</p>
+            <iframe style = {{width: '100%', marginTop: '1em',height: '444px'}}src="https://docs.google.com/presentation/d/e/2PACX-1vQp6SSpqkq5FbVBbk2KXJ8XyXTmIV09i7-uMQ9IvwEK29oYq9I_v8BkWD0gyxDCJMeO4BCwT4_i7V32/embed?start=false&loop=false&delayms=10000" frameBorder={0}  allowFullScreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" />
 
         </div>
     )
